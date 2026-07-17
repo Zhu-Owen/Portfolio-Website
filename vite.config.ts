b0e711593ebac4f8a83176b349f31b3289c06223
Owen Zhu <owen.zhu2012@gmail.com>
2026-07-16 22:19:50 -0400

    Fix 404
    Fix 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
+    nitro(),
     react(),
     tailwindcss(),
   ],
